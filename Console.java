72f015e7e4861c62f7afcdf21f5514c9a4864c87
lucas1matheus <161655700+lucas1matheus@users.noreply.github.com>
2024-11-29 16:00:13 -0300

    Update Console.java

--- Console.java
+++ Console.java
@@ -10,12 +10,13 @@ public class Console{
 
   public int menu(){
     
-    System.out.println("1 - adicionar Parcela");
-    System.out.println("2 - Remover pessoa");
-    System.out.println("3 - Alterar pessoa");
-    System.out.println("4 - Obter pessoa");
-    System.out.println("5 - ListarPessoas");
-    System.out.println("6 - Sair");
+    System.out.println("1 - Adicionar Parcela");
+    System.out.println("2 - Regar Parcela");
+    System.out.println("3 - Adubar Parcela");
+    System.out.println("4 - Marcar Tratamento Especial");
+    System.out.println("5 - Sugerir Acoes");
+    System.out.println("6 - Gerar Relatório");
+    System.out.println("0 - Sair");
     int resposta = leitor.nextInt();
     leitor.nextLine();
     return(resposta);
